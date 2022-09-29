2f35d5e1f376c9bb5cd6732e9e6c25c1edeb59e8
Paolo Politi <paolopoliti85@gmail.com>
2022-09-29 12:10:33 +0200

    - Switched from gunicorn to uwsgi for taiga-protected module - Increased default value for MAX_AGE to 300 seconds

--- scripts/dev_taiga_protected.sh
+++ scripts/dev_taiga_protected.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# Before proceeding make sure you have already activated virtual environment in ./taiga-back/.venv
+
 cd taiga-back
 pip install git+https://github.com/kaleidos-ventures/taiga-contrib-protected.git@6.5.0#egg=taiga-contrib-protected
 cd ..
 
 cd taiga-protected
 git checkout stable
+python3 -m venv .venv --prompt taiga-protected
+source .venv/bin/activate
+
 pip install -r requirements.txt
-cd ..
 
-cp ./config/env.example ./taiga-protected/.env
+cp ../config/env.example .env
 
-cd taiga-protected
-../.venv/bin/gunicorn --daemon --workers 4 --timeout 60 --log-level=info --access-logfile - --bind 0.0.0.0:8003 server:app
+# .venv/bin/gunicorn --daemon --workers 4 --timeout 60 --log-level=info --access-logfile taigaprotected.log --bind 127.0.0.1:8003 server:app
+
+pip install uwsgi
+.venv/bin/uwsgi --http 127.0.0.1:8003 --module server:app --daemonize taigaprotected.log
